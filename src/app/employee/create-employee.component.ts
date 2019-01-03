ce4e239db3152bcde850256595a942296f7139ba
Noorjahan Mukammel <jahan@Noorjahans-MacBook-Pro.local>
2019-01-03 17:28:12 +0600

    FormBuilder Service Used

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from "@angular/forms";
+import { FormGroup, FormControl, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-create-employee',
@@ -9,7 +9,7 @@ import { FormGroup, FormControl } from "@angular/forms";
 export class CreateEmployeeComponent implements OnInit {
 
 	employeeForm: FormGroup;
-	constructor() { }
+	constructor(private fb: FormBuilder) { }
 
 	ngOnInit() {
 		// ------------- Basics Completed -------------  //
@@ -20,16 +20,28 @@ export class CreateEmployeeComponent implements OnInit {
 		// ------------- Basics Completed -------------  //
 
 		// ------------- NestedForm Group ------------- //
-		this.employeeForm = new FormGroup({
-			fullName: new FormControl(),
-			email: new FormControl(),
-			skills: new FormGroup({
-				skillName: new FormControl(),
-				experienceInYears: new FormControl(),
-				proficiency: new FormControl()
+		// this.employeeForm = new FormGroup({
+		// 	fullName: new FormControl(),
+		// 	email: new FormControl(),
+		// 	skills: new FormGroup({
+		// 		skillName: new FormControl(),
+		// 		experienceInYears: new FormControl(),
+		// 		proficiency: new FormControl()
+		// 	})
+		// })
+		// ------------- NestedForm Group ------------- //
+
+		// ------------ Using FormBuilder Service ------------ //
+		this.employeeForm = this.fb.group({
+			fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
+			email: [''],
+			skills: this.fb.group({
+				skillName: [''],
+				experienceInYears: [''],
+				proficiency: ['beginner']
 			})
 		})
-		// ------------- NestedForm Group ------------- //
+		// ------------ Using FormBuilder Service ------------ //
 	}
 
 	onSetValueClick(): void
